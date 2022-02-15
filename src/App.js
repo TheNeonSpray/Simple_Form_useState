e15f4f5297dc286dc879154741263f076ef912c3
TheNeonSpray <alexandraeb20@gmail.com>
2022-02-15 12:20:55 -0600

    Basic exercise to practice useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+//import the state from react library
+import { useState } from "react";
 
 function App() {
+  //set state variables, this is where you store the changed data and the function(setter) that update that data.
+  //Destructuring
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //This function handles the value of the email input and pass it to the setter
+  //Destructuring of target and value, both are objects
+  const handleEmail = ({ target: { value } }) => setEmail(value);
+  //You don't need the braces when there's only one value
+  const handlePassword = ({ target: { value } }) => setPassword(value);
+  // const handleEmail = (e) => {
+  //   setEmail(e.target.value);
+  // };
+
+  // const handlePassword = (e) => {
+  //   setPassword(e.target.value)
+  // };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    alert(`User: ${email}\npassword:${password}`)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleFormSubmit}>
+        <h1>Login User</h1>
+        <label>
+          Email
+          {/* value is a state attribute */}
+          {/* onChange receives the data on real time */}
+          <input type="email" value={email} onChange={handleEmail} />
+        </label>
+        <label>
+          Password
+          <input type="password" value={password} onChange={handlePassword} />
+        </label>
+        <button type="submit">Enter</button>
+      </form>
     </div>
   );
 }
